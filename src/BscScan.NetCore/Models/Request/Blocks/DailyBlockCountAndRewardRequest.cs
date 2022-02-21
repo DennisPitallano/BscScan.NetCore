a2ccc0f150926eda8452f0943242fccb8806604e
DennisPitallano <dennis.pitallano@hotmail.co.uk>
2022-02-21 20:12:37 +0800

    Add  Get Daily Block Count and Rewards
    Returns the number of blocks validated daily and the amount of block rewards.

---
+++ src/BscScan.NetCore/Models/Request/Blocks/DailyBlockCountAndRewardRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BscScan.NetCore.Models.Request.Blocks
+{
+    /// <summary>
+    /// DailyBlockCountAndRewardRequest Model
+    /// </summary>
+    public  class DailyBlockCountAndRewardRequest: DailyBlockRequest
+    {
+    }
+}
---
+++ src/BscScan.NetCore/Models/Response/Blocks/DailyBlockCountAndRewards.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BscScan.NetCore.Models.Response.Blocks
+{
+    /// <summary>
+    /// DailyBlockCountAndRewards 
+    /// </summary>
+    public class DailyBlockCountAndRewards : BaseResponse
+    {
+        /// <summary>
+        /// List of DailyBlockCountAndRewardsData
+        /// </summary>
+        [JsonPropertyName("result")]
+        public IEnumerable<DailyBlockCountAndRewardsData>? Result { get; set; }
+    }
+
+    /// <summary>
+    /// DailyBlockCountAndRewardsData 
+    /// </summary>
+    public class DailyBlockCountAndRewardsData
+    {
+        /// <summary>
+        /// UTCDate
+        /// </summary>
+        [JsonPropertyName("UTCDate")]
+        public string? UtcDate { get; set; }
+
+        /// <summary>
+        /// UnixTimeStamp
+        /// </summary>
+        [JsonPropertyName("unixTimeStamp")]
+        public string? UnixTimeStamp { get; set; }
+
+        /// <summary>
+        /// BlockTimeSec
+        /// </summary>
+        [JsonPropertyName("blockTime_sec")]
+        public string? BlockTimeSec { get; set; }
+    }
+}
