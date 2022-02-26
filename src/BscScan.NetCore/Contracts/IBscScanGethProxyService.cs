44a0d13d666c018b0a0184a33ba87bf990a3e392
DennisPitallano <dennis.pitallano@hotmail.co.uk>
2022-02-26 23:01:21 +0800

    Add eth_getBlockByNumber
    Returns information about a block by block number.

--- src/BscScan.NetCore/Contracts/IBscScanGethProxyService.cs
+++ src/BscScan.NetCore/Contracts/IBscScanGethProxyService.cs
@@ -14,5 +14,14 @@ namespace BscScan.NetCore.Contracts
         /// Returns the number of most recent block
         /// </returns>
         Task<EthBlockNumber?> EthBlockNumber();
+
+        /// <summary>
+        /// eth_getBlockByNumber
+        /// </summary>
+        /// <param name="tag">the block number, in hex eg. 0xC36B3C</param>
+        /// <param name="boolean">the boolean value to show full transaction objects.
+        /// when true, returns full transaction objects and their information, when false only returns a list of transactions.</param>
+        /// <returns>Returns information about a block by block number.</returns>
+        Task<BlockByNumber?> EthGetBlockByNumber(string tag);
     }
 }
