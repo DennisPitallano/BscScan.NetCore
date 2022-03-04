a43a02a205ba87dc4aad216a56516b4c9fadd901
DennisPitallano <dennis.pitallano@hotmail.co.uk>
2022-03-04 21:02:56 +0800

    Add Get Daily Average Gas Limit
    Returns the historical daily average gas limit of the BNB Smart Chain network.

---
+++ src/BscScan.NetCore/Models/Response/GasTracker/DailyAverageGasLimit.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BscScan.NetCore.Models.Response.GasTracker;
+
+/// <summary>
+/// DailyAverageGasLimit
+/// </summary>
+public class DailyAverageGasLimit : BaseResponse
+{
+    /// <summary>
+    /// List of DailyAverageGasLimitData
+    /// </summary>
+    [JsonPropertyName("result")]
+    public IEnumerable<DailyAverageGasLimitData>? Result { get; set; }
+}
+
+/// <summary>
+/// DailyAverageGasLimitData
+/// </summary>
+public class DailyAverageGasLimitData
+{
+    /// <summary>
+    /// UTCDate
+    /// </summary>
+    [JsonPropertyName("UTCDate")]
+    public string? UtcDate { get; set; }
+
+    /// <summary>
+    /// unixTimeStamp
+    /// </summary>
+    [JsonPropertyName("unixTimeStamp")]
+    public string? UnixTimeStamp { get; set; }
+
+    /// <summary>
+    /// gasLimit
+    /// </summary>
+    [JsonPropertyName("gasLimit")]
+    public string? GasLimit { get; set; }
+}
--- src/BscScan.NetCore/Services/BscScanGasTrackerService.cs
+++ src/BscScan.NetCore/Services/BscScanGasTrackerService.cs
@@ -3,6 +3,7 @@ using BscScan.NetCore.Configuration;
 using BscScan.NetCore.Constants;
 using BscScan.NetCore.Contracts;
 using BscScan.NetCore.Extensions;
+using BscScan.NetCore.Models.Request.GasTracker;
 using BscScan.NetCore.Models.Response.GasTracker;
 
 namespace BscScan.NetCore.Services;
@@ -12,11 +13,13 @@ public class BscScanGasTrackerService : BaseHttpClient, IBscScanGasTrackerServic
 {
 
     private readonly string _bscScanGasTrackerModule;
+    private readonly string _bscScanStatsModule;
     /// <inheritdoc />
     public BscScanGasTrackerService(HttpClient bscScanHttpClient, BscScanConfiguration bscScanConfiguration)
         : base(bscScanHttpClient, bscScanConfiguration)
     {
         _bscScanGasTrackerModule = BscScanModule.GAS_TRACKER.AppendApiKey(bscScanConfiguration.BscScanOptions.Token);
+        _bscScanStatsModule = BscScanModule.STATS.AppendApiKey(bscScanConfiguration.BscScanOptions.Token);
     }
 
     /// <inheritdoc />
@@ -29,6 +32,19 @@ public class BscScanGasTrackerService : BaseHttpClient, IBscScanGasTrackerServic
         response.EnsureSuccessStatusCode();
         await using var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
         var result = await JsonSerializer.DeserializeAsync<GasOracle>(responseStream);
+        return result;
+    }
+
+    /// <inheritdoc />
+    public async Task<DailyAverageGasLimit?> GetDailyAverageGasLimit(DailyAverageGasLimitRequest request)
+    {
+        var queryParameters = $"{_bscScanStatsModule}{request.ToRequestParameters(GasTrackerModuleAction.DAILY_AVG_GAS_LIMIT)}";
+        using var response = await BscScanHttpClient.GetAsync($"{queryParameters}")
+            .ConfigureAwait(false);
+
+        response.EnsureSuccessStatusCode();
+        await using var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+        var result = await JsonSerializer.DeserializeAsync<DailyAverageGasLimit>(responseStream);
         return result;
     }
 }
