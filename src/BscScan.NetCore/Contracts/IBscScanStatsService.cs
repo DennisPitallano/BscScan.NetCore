71f6617c98bf25b3ec28c7670d98109b77fcdb86
DennisPitallano <dennis.pitallano@hotmail.co.uk>
2022-03-04 22:08:07 +0800

    Get Total Supply of BNB on the BNB Smart Chain
    Returns the current amount of BNB in circulation.

---
+++ src/BscScan.NetCore/Contracts/IBscScanStatsService.cs
@@ -0,0 +1,15 @@
+﻿using BscScan.NetCore.Models.Response.Stats;
+
+namespace BscScan.NetCore.Contracts;
+
+/// <summary>
+///  BscScan Stats Module Interface
+/// </summary>
+public interface IBscScanStatsService
+{
+    /// <summary>
+    /// Get Total Supply of BNB on the BNB Smart Chain
+    /// </summary>
+    /// <returns>Returns the current amount of BNB in circulation.</returns>
+    Task<TotalSupply> GetTotalSupply();
+}
---
+++ src/BscScan.NetCore/Models/Response/Stats/TotalSupply.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BscScan.NetCore.Models.Response.Stats
+{
+    /// <summary>
+    /// TotalSupply
+    /// </summary>
+    public class TotalSupply : BaseResponse
+    {
+        /// <summary>
+        /// result
+        /// </summary>
+        [JsonPropertyName("result")]
+        public string? Result { get; set; }
+    }
+}
